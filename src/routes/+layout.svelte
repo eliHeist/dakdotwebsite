c5f2646e3bc4fc543cde76f4dcb317fc922ff4f2
Elijah Triumph <eliyang256@outlook.com>
2025-01-29 01:58:07 +0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+	import '../app.css';
+    import '../app.ts';
+
+    import Lenis from "lenis";
+    import { gsap } from "gsap";
+
+    $effect(() => {
+        // const ScrollTrigger = (await import('gsap/ScrollTrigger')).default;
+
+        // Initialize a new Lenis instance for smooth scrolling
+        const lenis = new Lenis();
+
+        // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+        // lenis.on('scroll', ScrollTrigger.update);
+
+        // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+        // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+        gsap.ticker.add((time) => {
+        lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+        });
+
+        // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+        gsap.ticker.lagSmoothing(0);
+	});
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		{@render children()}
+	</main>
+
+	<footer>
+		<p>
+            DakDot Creations &copy; 2025
+		</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcomeFallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Dakdot homepage, landing page" />
+</svelte:head>
+
+<section class="h-[91vh] grid items-center">
+	<div class="hero-text">
+        <div class="one">DIGITAL</div>
+        <div class="two">SOLUTIONS</div>
+        <div class="three">AGENCY</div>
+    </div>
+</section>
+
+<style>
+    
+    .hero-text {
+        font-size: 8rem;
+        font-weight: 900;
+
+        & .two{
+            text-align: center;
+            color: var(--color-red);
+        }
+
+        & .three{
+            text-align: right;
+        }
+    }
+</style>
