79b7a0ac60fbc1f07684f296d6a86a9872a700e2
Elijah Triumph <eliyang256@outlook.com>
2025-03-02 22:56:38 +0300

    about page boller

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,25 @@
+<script lang="ts">
+    import { getContext } from 'svelte';
+    import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+
+	let spacer: HTMLElement;
+
+	$effect(() => {
+		let headerHeight = getContext('header_height');
+		spacer.style.paddingTop = `${headerHeight}px`;
+	});
+</script>
+
 <svelte:head>
 	<title>About</title>
-	<meta name="description" content="About this app" />
+	<meta name="description" content="About Dakdot creations" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://svelte.dev/docs/kit">SvelteKit</a> app. You can make your own by typing
-		the following into your command line and following the prompts:
-	</p>
-
-	<pre>npx sv create</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+<div class="content-grid pt-[10vh] md:pt-[20vh]" bind:this={spacer}>
+	<!-- Hero Section -->
+	<section class="text-center h-[50vh] grid place-content-center justify-items-center">
+		<h1 class="title-2">We are a Digital Agency specialized in custom business solutions</h1>
+	</section>
 </div>
